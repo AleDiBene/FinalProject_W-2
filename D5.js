e45c63634beed4e65c464de659c5473819d237a3
Alessandro Di Benedetto <socialorne02@gmail.com>
2025-02-14 12:04:05 +0100

    first exercise completed

--- D5.js
+++ D5.js
@@ -12,6 +12,10 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
+for(let i = 0; i < pets.length; i++){
+  console.log(pets[i])
+}
+
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
